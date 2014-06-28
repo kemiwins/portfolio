f7150f75e93ea6b39d266d77526671f2f54293c8
kemiwins <kemi.mail@gmail.com>
2014-06-28 09:29:36 -0400

    removed page scrolling w/view button

--- app.js
+++ app.js
@@ -2,9 +2,5 @@ $(document).ready(function() {
 
 	$("#view").click(function(){
 		$("#addProjects").slideToggle();
-		$(document.body).animate({
-			"scrollTop": $("#projects").offset().top
-		}, 500);
 	})
-
 });
